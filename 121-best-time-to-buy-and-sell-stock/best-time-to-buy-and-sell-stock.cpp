19d4c5f64fb18656d04e5ff5b161ad2cdd0b3bce
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-08-08 19:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 97.6 MB (7.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int maxprof=0;
         int minbuy = prices[0];
         for(int i = 1; i<prices.size(); i++){
-            if(prices[i]-minbuy>maxprof) maxprof= prices[i]-minbuy;
-            if(prices[i]<minbuy) minbuy = prices[i];
+            maxprof = max(prices[i]-minbuy,maxprof);
+            minbuy = min(minbuy, prices[i]);
         }
         return maxprof;
     }
