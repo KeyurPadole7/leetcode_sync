3620e860987fb9ac1ba615e33aa0054be817f94c
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-08-08 19:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.53%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long m = n;
+        if(n<0){
+            x = 1/x;
+            m = 0-m;
+        }
+
+        long long binform = m;
+        double ans = 1;
+
+        while(binform>0){
+            if(binform%2!=0){
+                ans *= x;
+            }
+            x *= x;
+            binform /= 2;
+        }
+
+        return ans;
     }
 };
