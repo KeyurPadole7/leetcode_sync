c7005afa00d78837c07370aaa7c5c01fb9b37f42
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-08-09 12:28:25 +0530

    Time: 5 ms (51.50%) | Memory: 23.7 MB (82.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -9,7 +9,7 @@ public:
         ans.push_back(intervals[0]);
 
         for(int i = 0; i < intervals.size(); i++){
-            if(ans[ans.size()-1][1]>=intervals[i][0]) ans[ans.size()-1][1] = max(intervals[i][1],ans[ans.size()-1][1]) ;
+            if(ans.back()[1]>=intervals[i][0]) ans.back()[1] = max(intervals[i][1],ans.back()[1]) ;
             else ans.push_back(intervals[i]);
         }
 
