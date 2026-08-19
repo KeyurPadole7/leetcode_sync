45d5df0cb193c56bc48dcd7aea14375f48350914
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-08-19 18:55:52 +0530

    Time: 2 ms (36.67%) | Memory: 12.8 MB (20.87%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> bkt;
 
-        fss(nums, bkt, ans, 0);
+        fss(nums, bkt, 0);
         return ans;
     }
 
-    void fss(vector<int>& nums, vector<int>& bkt, vector<vector<int>>& ans, int i){
+    void fss(vector<int>& nums, vector<int>& bkt, int i){
         if(i == nums.size()){
             ans.push_back({bkt});
             return;
         }
 
-        fss(nums, bkt, ans, i+1);
+        fss(nums, bkt, i+1);
 
         bkt.push_back(nums[i]);
-        fss(nums, bkt, ans, i+1);
+        fss(nums, bkt, i+1);
 
         bkt.pop_back();
         
