39ea6a3766ffbab2db4522a4b50ccb2e8468d244
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-09-03 15:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -3,12 +3,13 @@ public:
     bool uniformArray(vector<int>& nums1) {
         if(nums1.size() == 1) return true;
         int mn = INT_MAX;
-        for(int val: nums1) mn = min(mn, val);
+        bool alleven = true;
+        for(int val: nums1){
+            mn = min(mn, val);
+            if(val%2 == 1) alleven = false;
+        }
 
         if(mn%2 == 1) return true;
-
-        for(int val: nums1) if(val%2 == 1) return false;
-
-        return true;
+        return alleven? true: false;
     }
 };
