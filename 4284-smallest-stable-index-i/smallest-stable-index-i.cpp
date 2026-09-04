96f5ceb6c56abcbc427117d6f0eb4ddc11a4c995
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-09-04 18:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (20.68%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> premax;
+        vector<int> premax(n);
         vector<int> postmin(n);
 
-        premax.push_back(nums[0]);
+        premax[0] = nums[0];
         postmin[n-1] = nums[n-1];
 
         for(int i=1; i<n; i++){
-            premax.push_back(max(premax.back(), nums[i]));
+            premax[i] = max(premax[i-1], nums[i]);
             postmin[n-i-1] = min(postmin[n-i], nums[n-i-1]);
         }
 
