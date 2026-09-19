3c1c6662c1cad42905bda3cd1a3c9921f1696f02
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-09-19 11:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.38%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xc, int yc, int x1, int y1, int x2, int y2) {
+        int nrstx = max(x1, min(xc, x2));
+        int nrsty = max(y1, min(yc, y2));
+
+        int dx = nrstx - xc;
+        int dy = nrsty - yc;
+
+        return (dx*dx + dy*dy) <= radius*radius;
+    }
+};
