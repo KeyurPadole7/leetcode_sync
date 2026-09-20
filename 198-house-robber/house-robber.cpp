30ebf7c5e1783f9b3fae5ecf69f8a778edb729c7
Keyur Sanjay Padole <keyurpadole@gmail.com>
2026-09-21 00:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.35%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,16 +5,13 @@ public:
         if (n == 1)
             return nums[0];
 
-        if (n == 2)
-            return max(nums[0], nums[1]);
-
         // vector<int> dp(n);
         // dp[0] = nums[0];
         // dp[1] = max(nums[0], nums[1]);
 
-        int curr;
         int prev1 = max(nums[0], nums[1]);
         int prev2 = nums[0];
+        int curr = prev1;
 
         for (int i = 2; i < n; i++) {
             curr = max(prev1, prev2 + nums[i]);
